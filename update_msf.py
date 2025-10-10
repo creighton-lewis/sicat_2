7a9bd696659d1957aee36392a8b5c13bde889646
cl <creightonlewis01@gmail.com>
2025-10-10 17:02:45 -0500

    Delete update_msf.py

--- update_msf.py
+++
@@ -1,9 +0,0 @@
-import os 
-import subprocess 
-def get_url():
-  os.system
-  os.system(f"curl https://raw.githubusercontent.com/rapid7/metasploit-framework/master/db/modules_metadata_base.json >> msf_module.json")
-def convert():
-  import json_convert 
-get_url()
-convert()
