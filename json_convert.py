51c500a67630a69a48f3e4f654aa0b08aca4c48a
cl <creightonlewis01@gmail.com>
2025-10-10 16:58:13 -0500

    Update json_convert.py

--- json_convert.py
+++ json_convert.py
@@ -5,10 +5,7 @@ import subprocess
 def get_url():
   os.system
   os.system(f"curl https://raw.githubusercontent.com/rapid7/metasploit-framework/master/db/modules_metadata_base.json >> msf_module.json")
-def convert():
-  import json_convert 
 get_url()
-convert()
 
 msf_module = os.path.abspath('msf_module.json')
 print(msf_module)
