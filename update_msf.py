24a7bbe71cd5a5b6b79f16177314c22fbe6b9ac6
cl <creightonlewis01@gmail.com>
2025-10-10 15:54:00 -0500

    Update update_msf.py

--- update_msf.py
+++ update_msf.py
@@ -4,4 +4,5 @@ def get_url():
   os.system
   os.system(f"curl https://raw.githubusercontent.com/rapid7/metasploit-framework/master/db/modules_metadata_base.json")
 def convert():
+  
   import json_convert 
