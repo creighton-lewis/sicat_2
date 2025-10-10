8cb9502111ebbe55c68b6df2041e7ac479ef8383
cl <creightonlewis01@gmail.com>
2025-10-10 15:53:28 -0500

    Update and rename msf_module_original to update_msf.py

---
+++ update_msf.py
@@ -0,0 +1,7 @@
+import os 
+import subprocess 
+def get_url():
+  os.system
+  os.system(f"curl https://raw.githubusercontent.com/rapid7/metasploit-framework/master/db/modules_metadata_base.json")
+def convert():
+  import json_convert 
